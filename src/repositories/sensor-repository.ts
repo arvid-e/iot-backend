46acd726b9b2aed62614074251f0302c7a096d15
arvid-e <arvid.edvinsson@gmail.com>
2026-05-13 09:24:14 +0900

    Setup dependency injection in sensor route

--- src/repositories/sensor-repository.ts
+++ src/repositories/sensor-repository.ts
@@ -3,11 +3,13 @@ import type { SensorRepository } from '../interfaces/sensor-repository.js';
 import SensorDataModel from '../models/SensorData.js';
 
 export class SensorRepositoryImpl implements SensorRepository {
+  constructor(private sensorDataModel: typeof SensorDataModel) {}
+
   async findAll(limit = 100): Promise<SensorData[]> {
-    return await SensorDataModel.find().lean().limit(limit);
+    return await this.sensorDataModel.find().lean().limit(limit);
   }
 
   async findLatest(): Promise<SensorData | null> {
-    return await SensorDataModel.findOne().sort({ createdAt: -1 }).lean();
+    return await this.sensorDataModel.findOne().sort({ createdAt: -1 }).lean();
   }
 }
--- src/routes/sensor-routes.ts
+++ src/routes/sensor-routes.ts
@@ -1,8 +1,17 @@
 import { Router } from 'express';
 
+import { SensorController } from '../controllers/sensor-controller.js';
+import SensorDataModel from '../models/SensorData.js';
+import { SensorRepositoryImpl } from '../repositories/sensor-repository.js';
+import { SensorServiceImpl } from '../services/sensor-service.js';
+
+const sensorRepository = new SensorRepositoryImpl(SensorDataModel);
+const sensorService = new SensorServiceImpl(sensorRepository);
+const sensorController = new SensorController(sensorService);
+
 const router = Router();
 
-// router.get('/', sensorController.getHistoricalData);
-// router.get('/latest', sensorController.getLatestData);
+router.get('/', sensorController.getHistoricalData);
+router.get('/latest', sensorController.getLatestData);
 
 export default router;
